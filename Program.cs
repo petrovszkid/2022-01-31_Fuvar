92e0d27532f282a3f2af5fcd05fdc97e5f61bccb
petrovszkid <petrovszkid.12d@acsjszki.hu>
2022-01-31 13:07:33 +0100

    2.feladat

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace fuvar
 {
     class Program
     {
+
         //1.feladat
 
         struct fuvarozas
@@ -22,6 +23,39 @@ namespace fuvar
             public string fizetesmod;
         }
 
-        
+        //2.feladat
+
+        static fuvarozas[] adatok = new fuvarozas[2000];
+        struct fuvarozas2
+        {
+            public int taxiid;
+            public string indulas;
+            public int idotartam;
+            public double tavolsag;
+            public double viteldij;
+            public double borravalo;
+            public string fizetesmod;
+        }
+        static fuvarozas2[] adatok2 = new fuvarozas2[2000];
+        static void Main(string[] args)
+        {
+            string[] fajlbol = File.ReadAllLines("fuvar.csv");
+            int sorokszama = 0;
+            int i, j;
+            for (int k = 1; k < fajlbol.Count(); k++)
+            {
+                string[] egysordarabolva = fajlbol[k].Split(';');
+                adatok[sorokszama].taxiid = Convert.ToInt32(egysordarabolva[0]);
+                adatok[sorokszama].indulas = egysordarabolva[1];
+                adatok[sorokszama].idotartam = Convert.ToInt32(egysordarabolva[2]);
+                adatok[sorokszama].tavolsag = Convert.ToDouble(egysordarabolva[3]);
+                adatok[sorokszama].viteldij = Convert.ToDouble(egysordarabolva[4]);
+                adatok[sorokszama].borravalo = Convert.ToDouble(egysordarabolva[5]);
+                adatok[sorokszama].fizetesmod = egysordarabolva[6];
+                sorokszama++;
+            }
+            int fuvarokszama = sorokszama;
+
+
+        }
     }
-}
