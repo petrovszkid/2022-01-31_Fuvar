5f977a892b0132b5e8752a43e1609930a40223f4
petrovszkid <petrovszkid.12d@acsjszki.hu>
2022-02-04 12:41:26 +0100

    6-7-8.feladat

--- Program.cs
+++ Program.cs
@@ -108,7 +108,92 @@ namespace fuvar
             for (i = 1; i <= kulonbozoelemekszama; i++)
                 Console.WriteLine("\t{0}: {1} fuvar ", fizetesimodok[i], fizetesszam[i]);
 
+            //6.feladat
 
+            double osszesmerfold = 0;
+            for (i = 0; i < fuvarokszama; i++)
+            {
+                osszesmerfold += adatok[i].tavolsag;
+            }
+            Console.WriteLine("6. feladat: {0} km", Math.Round(osszesmerfold * 1.6, 2));
+
+            //7.feladat
+
+            int maxfuvar = adatok[0].idotartam;
+            int maxi = 0;
+            for (i = 0; i < fuvarokszama; i++)
+            {
+                if (adatok[i].idotartam > maxfuvar)
+                {
+                    maxfuvar = adatok[i].idotartam;
+                    maxi = i;
+                }
+            }
+            Console.WriteLine("7. feladat: Leghosszabb\n\tFuvar hossza: {0} másodperc", adatok[maxi].idotartam);
+            Console.WriteLine("\tTaxi azonosító: {0}", adatok[maxi].taxiid);
+            Console.WriteLine("\tMegtett távolság: {0} km", adatok[maxi].tavolsag * 1.6);
+            Console.WriteLine("\tViteldíj: {0} $", adatok[maxi].viteldij);
+
+            //8.feladat
+
+            Console.WriteLine("8. feladat: hibak.txt");
+
+            int idb = 0;
+            for (i = 0; i < fuvarokszama; i++)
+            {
+                if (adatok[i].idotartam > 0 && adatok[i].viteldij > 0 && adatok[i].tavolsag == 0)
+                {
+                    adatok2[idb].taxiid = adatok[i].taxiid;
+                    adatok2[idb].indulas = adatok[i].indulas;
+                    adatok2[idb].idotartam = adatok[i].idotartam;
+                    adatok2[idb].tavolsag = adatok[i].tavolsag;
+                    adatok2[idb].viteldij = adatok[i].viteldij;
+                    adatok2[idb].borravalo = adatok[i].borravalo;
+                    adatok2[idb].fizetesmod = adatok[i].fizetesmod;
+                    idb++;
+                }
+            }
+            int hibakszama = idb;
+
+            int mini = 0;
+            string min;
+            for (i = 0; i < hibakszama; i++)
+            {
+                mini = i;
+                min = adatok2[i].indulas;
+                for (j = i; j < hibakszama; j++)
+                {
+                    int x = String.Compare(adatok2[j].indulas, min);
+                    if (x < 0)
+                    {
+                        mini = j;
+                        min = adatok2[j].indulas;
+                    }
+                }
+
+                var s = adatok2[i];
+                adatok2[i] = adatok2[mini];
+                adatok2[mini] = s;
+            }
+
+            FileStream fnev = new FileStream("hibak.txt", FileMode.Create);
+            StreamWriter fajlbairo = new StreamWriter(fnev);
+            fajlbairo.WriteLine("taxi_id;indulas;idotartam;tavolsag;viteldij;borravalo;fizetes_modja");
+            for (i = 0; i < hibakszama; i++)
+            {
+                fajlbairo.Write("{0};", adatok2[i].taxiid);
+                fajlbairo.Write("{0};", adatok2[i].indulas);
+                fajlbairo.Write("{0};", adatok2[i].idotartam);
+                fajlbairo.Write("{0};", adatok2[i].tavolsag);
+                fajlbairo.Write("{0};", adatok2[i].viteldij);
+                fajlbairo.Write("{0};", adatok2[i].borravalo);
+                fajlbairo.Write("{0}", adatok2[i].fizetesmod);
+                fajlbairo.WriteLine("\n");              
+            }
+            fajlbairo.Close();
+            fnev.Close();
+
+            Console.ReadKey();
         }
     }
 }
